555d7d3cc346feacca414278fe0b8db95f8cd12f
Akio Olvera <akiooarao@gmail.com>
2022-04-30 10:25:09 -0600

    Adding form fields, validation and error message

--- src/pages/compund-interest-calculator/calculator-input-form/CalculatorInputForm.js
+++ src/pages/compund-interest-calculator/calculator-input-form/CalculatorInputForm.js
@@ -1,11 +1,98 @@
-import { TextField } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert, Button, InputAdornment, TextField } from '@mui/material';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import PercentIcon from '@mui/icons-material/Percent';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import styles from './CalculatorInputForm.module.css';
+import _ from 'lodash'
 
 const CalculatorInputForm = () => {
+  const [initialDeposit, setInitialDeposit] = useState(0);
+  const [yearlyInterestRate, setYearlyInterestRate] = useState(0);
+  const [yearsToInvest, setYearsToInvest] = useState(0);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const onInitialDepositChange = ({ target: { value } }) => {
+    if (_.isNaN(Number(value))) {
+      return;
+    }
+    setInitialDeposit(value);
+  };
+  const onYearlyInterestRateChange = ({ target: { value } }) => {
+    if (_.isNaN(Number(value))) {
+      return;
+    }
+    setYearlyInterestRate(value);
+  };
+  const onYearsToInvestChange = ({ target: { value } }) => {
+    if (!_.isInteger(Number(value))) {
+      return;
+    }
+    setYearsToInvest(value);
+  };
+  const onButtonClicked = () => {
+    if (initialDeposit === '' || yearlyInterestRate === '' || yearsToInvest === '') {
+      setErrorMessage('Some fields are missing');
+      return;
+    }
+    console.log('calculating');
+  };
+
   return (
-    <div>
+    <div className={styles.form}>
       <h3>Enter your data</h3>
-      <TextField label="Initial Deposit" />
+      {
+        errorMessage
+        && (
+          <Alert
+            severity="error"
+            onClose={() => setErrorMessage('')}
+          >
+            Some fields are missing
+          </Alert>)
+      }
+      <TextField
+        label="Initial Deposit"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AttachMoneyIcon />
+            </InputAdornment>
+          )
+        }}
+        value={initialDeposit}
+        onChange={onInitialDepositChange}
+      />
+      <TextField
+        label="Yearly Interest Rate"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <PercentIcon />
+            </InputAdornment>
+          )
+        }}
+        value={yearlyInterestRate}
+        onChange={onYearlyInterestRateChange}
+      />
+      <TextField
+        label="Yearly To Invest"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <CalendarTodayIcon />
+            </InputAdornment>
+          )
+        }}
+        value={yearsToInvest}
+        onChange={onYearsToInvestChange}
+      />
+      <Button
+        variant="contained"
+        onClick={onButtonClicked}
+      >
+        Calculate
+      </Button>
     </div>
   );
 }
